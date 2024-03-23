a7b448df5538a6926cf743b93cc88ff034af6bbf
Krevetych <126977263+Krevetych@users.noreply.github.com>
2024-03-23 10:48:36 +0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -14,12 +14,20 @@ export const data = [
 		img: './github.png',
 		isDisabled: false,
 	},
-
+	
 	{
 		id: 3,
+		link: 'https://wsr.universal-hub.site',
+		username: 'WSR API',
+		img: './code.png',
+		isDisabled: false,
+	},
+
+	{
+		id: 4,
 		link: 'https://store.universal-hub.site',
 		username: 'Food store',
 		img: './code.png',
 		isDisabled: true,
-	},
+	}
 ]
